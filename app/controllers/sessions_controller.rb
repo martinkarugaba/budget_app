5fe4da72a571d5090fb30c4bf04bc04701b97c18
Martin Karugaba <martinkarugaba21@gmail.com>
2024-01-16 18:17:13 +0300

    Fix linter errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
 # app/controllers/sessions_controller.rb
 class SessionsController < Devise::SessionsController
-  skip_before_action :require_no_authentication, only: [:new, :create]
+  skip_before_action :require_no_authentication, only: %i[new create]
 end
