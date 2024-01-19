63fabe64c2e0aa1e71347d1d106436b5f0862fda
Martin Karugaba <martinkarugaba21@gmail.com>
2024-01-19 11:44:29 +0300

    Add unit tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Associations
   has_many :categories
   has_many :transactions, foreign_key: 'author_id'
 
-  validates :username, presence: true, uniqueness: true
+  # Validations
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 # spec/factories/users.rb
 FactoryBot.define do
   factory :user do
-    name { Faker::Internet.name }
-    email { Faker::Internet.email }
+    name { Faker::Internet.user_name }
+    email { Faker::Internet.unique.email }
     password { 'password123' }
   end
 end
