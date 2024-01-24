0737451a9adf6f1efe7b02bd6545e185eb44824c
Martin Karugaba <martinkarugaba21@gmail.com>
2024-01-24 17:22:11 +0300

    Fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,8 +68,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new(args: ['--headless', '--disable-gpu'])
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
 Capybara.javascript_driver = :headless_chrome
-
